3c7c05a1c0e67b459801ed32f9e6347efd0d6a09
Tobias Rueetschi <tobias.ruetschi@adfinis-sygroup.ch>
2017-01-17 09:49:17 +0100

    project setup instructions

---
+++ README.rst
@@ -0,0 +1,32 @@
+=============
+ACME PowerDNS
+=============
+
+
+INTRODUCTION
+============
+
+
+REQUIREMENTS
+============
+
+
+INSTALLATION
+============
+
+
+USAGE
+=====
+
+
+LICENSE
+=======
+
+GNU GENERAL PUBLIC LICENSE Version 3
+
+See the `LICENSE`_ file.
+
+.. _LICENSE: LICENSE
+
+
+.. vim: set ft=rst sw=2 ts=2 et wrap tw=76:
---
+++ setup.py
@@ -0,0 +1,41 @@
+from codecs import open as codecs_open
+from setuptools import setup, find_packages
+
+
+# Get the long description from the relevant file
+with codecs_open('README.rst', encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+
+setup(
+    name='acme_powerdns',
+    version='0.1',
+    description='ACME client with DNS challenge response with PowerDNS',
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    keywords='acme letsencrypt powerdns',
+    author='Adfinis SyGroup AG',
+    author_email='https://adfinis-sygroup.ch/',
+    url='https://github.com/adfinis-sygroup/acme-powerdns',
+    license='GPLv3',
+    packages=find_packages(),
+    install_requires=[
+        'acme',
+    ],
+    extras_require={
+        'test': ['pytest'],
+    },
+    entry_points="""
+    [console_scripts]
+    acme-powerdns=acme_powerdns.cli:main
+    """
+)
