df87f2fdcb2cc0b8e2f3638f9233248bf3eaf0ab
Tobias Rueetschi <tobias.ruetschi@adfinis-sygroup.ch>
2017-01-23 16:16:44 +0100

    class cli: use new acme_client class

--- acme_powerdns/cli.py
+++ acme_powerdns/cli.py
@@ -2,6 +2,7 @@
 
 import logging
 import sys
+from OpenSSL import crypto
 from acme_powerdns import acme_client
 from acme_powerdns import dns
 from acme_powerdns import settings
@@ -23,25 +24,42 @@ def main():
     # create an ACME account
     account_key = ac.create_account(settings.ACCOUNT_KEY)
 
-    # request a challenge
-    authzr, authzr_response = ac.request_challenges(settings.FQDN)
-    token = ac.filter_challenges(authzr, authzr_response)
+    authzrs = list()
+    for domain in settings.FQDN:
+        # request a challenge
+        authzr, authzr_response = ac.request_challenges(domain)
+        authzrs.append(authzr)
 
-    # create dns record
-    nsupdate.create(
-        settings.SERVER,
-        settings.ZONE,
-        settings.FQDN,
-        token,
-    )
+        challb = ac.filter_challenges(authzr, authzr_response)
+        chall_response, chall_validation = challb.response_and_validation(
+            account_key
+        )
 
-    # delete dns record
-    nsupdate.delete(
-        settings.SERVER,
-        settings.ZONE,
-        settings.FQDN,
-        token,
-    )
+        # create dns record
+        nsupdate.create(
+            settings.SERVER,
+            settings.ZONE,
+            '_acme-challenge.{}'.format(domain),
+            chall_validation,
+        )
+
+        try:
+            ac.answer_challenge(challb, chall_response)
+        except BaseException as e:
+            logging.error(e)
+
+        # delete dns record
+        nsupdate.delete(
+            settings.SERVER,
+            settings.ZONE,
+            '_acme-challenge.{}'.format(domain),
+            chall_validation,
+        )
+
+    (cert, chain) = ac.request_cert(settings.CSR, authzrs)
+    with open(settings.CRT, 'wb') as f:
+        for crt in cert:
+            f.write(crypto.dump_certificate(crypto.FILETYPE_PEM, crt))
 
     sys.exit(0)
 
