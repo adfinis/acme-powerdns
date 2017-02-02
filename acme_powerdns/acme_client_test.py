cba7f84a34242a1a66cbfe806674128e793194bf
Tobias Rueetschi <tobias.ruetschi@adfinis-sygroup.ch>
2017-02-02 13:05:55 +0100

    tests acme_client: added more tests

--- acme_powerdns/acme_client_test.py
+++ acme_powerdns/acme_client_test.py
@@ -3,18 +3,40 @@
 
 """Test for acme_client."""
 
+import pytest
+
 import logging
 import acme
 from acme_powerdns import acme_client
 
 
-def test_account():
-    logging.basicConfig(level=logging.INFO)
+@pytest.fixture(scope='session')
+def log():
+    logging.basicConfig(level=logging.WARNING)
+    return logging
+
+
+@pytest.fixture(autouse=True, scope='session')
+def account():
     ac = acme_client.Account(
-        logging,
+        log(),
         'https://acme-staging.api.letsencrypt.org/directory',
     )
-
     ac.create_account('.testenv/account.key')
+    return ac
 
-    assert not type(ac.get_regr()) == acme.messages.RegistrationResource
+
+def test_account_regr():
+    assert type(account().get_regr()) == acme.messages.RegistrationResource
+
+
+def test_account_client():
+    assert type(account().get_client()) == acme.client.Client
+
+
+def test_account_account_key():
+    assert type(account().get_account_key()) == acme.jose.jwk.JWKRSA
+
+
+def test_cert_request_request_token():
+    assert type(account().get_account_key()) == acme.jose.jwk.JWKRSA
