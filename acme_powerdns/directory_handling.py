e7af76f9ad49799b455bd49efd2b92d8890e3f4d
Tobias Rueetschi <tobias.ruetschi@adfinis-sygroup.ch>
2017-02-07 14:31:38 +0100

    class DirectoryHandling: added which validates each csr inside a directory and store the corresponding certificate

---
+++ acme_powerdns/directory_handling.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Copyright (c) 2017, Adfinis SyGroup AG
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import os
+
+from OpenSSL import crypto
+from acme_powerdns import acme_client, cert_handling
+
+
+class DirectoryHandling:
+
+    def __init__(self, logging, directory_url, account_key, csr_dir,
+                 cert_topdir, days=60, nsupdate=None):
+
+        self._logging = logging
+        self._directory_url = directory_url
+        self._account_key = account_key
+        self._days = days
+        self._nsupdate = nsupdate
+        self._csr_dir = csr_dir
+        self._cert_topdir = cert_topdir
+        self._ac = None
+
+    def get_account(self):
+
+        if self._ac is None:
+            self._ac = acme_client.Account(
+                self._logging,
+                self._directory_url,
+            )
+
+            # create an ACME account
+            self._ac.create_account(
+                self._account_key,
+            )
+
+        return self._ac
+
+    def handle(self):
+
+        for csr in os.listdir(self._csr_dir):
+            # calculate csr filename
+            csr = os.path.join(self._csr_dir, csr)
+            if not os.path.isfile(csr):
+                continue
+            self._logging.info(
+                'handle certificate signing request: {0}'.format(csr)
+            )
+
+            # get a cert handle
+            cert_handle = cert_handling.CertHandling()
+            cert_handle.set_csr(csr)
+
+            # calculate cert filename
+            cn = cert_handle.get_common_name()
+            crt = os.path.join(self._cert_topdir, cn, 'cert.pem')
+            self._logging.debug(
+                'certificate filename: {0}'.format(crt)
+            )
+
+            # calculate chain filename
+            chain = os.path.join(self._cert_topdir, cn, 'chain.pem')
+            self._logging.debug(
+                'certificate chain filename: {0}'.format(chain)
+            )
+
+            # calculate FQDNs
+            fqdn = cert_handle.get_alternative_names()
+            self._logging.debug(
+                'fqdn list: {0}'.format(fqdn)
+            )
+
+            # calculate enddate
+            enddate = cert_handle.enddate()
+            self._logging.info('Certificate {0} expires in {1} days'.format(
+                crt,
+                enddate,
+            ))
+            if enddate < self._days:
+                # create certificate request
+                cr = acme_client.CertRequest(self.get_account())
+                tokens = cr.request_tokens(
+                    fqdn,
+                    'dns01',
+                )
+
+                # create dns record
+                for token in tokens:
+                    self._nsupdate.create(
+                        '_acme-challenge.{0}'.format(token['domain']),
+                        token['validation'],
+                    )
+
+                # get cert and chain content
+                cert_content, chain_content = cr.answer_challenges(
+                    cert_handle._csr,
+                )
+
+                # write certificate content to file
+                with open(crt, 'wb') as f:
+                    for content in cert_content:
+                        f.write(crypto.dump_certificate(
+                            crypto.FILETYPE_PEM,
+                            content,
+                        ))
+                # write chain content to file
+                with open(chain, 'wb') as f:
+                    for content in chain_content:
+                        f.write(crypto.dump_certificate(
+                            crypto.FILETYPE_PEM,
+                            content,
+                        ))
+
+                # delete dns record
+                for token in tokens:
+                    self._nsupdate.delete(
+                        '_acme-challenge.{0}'.format(token['domain']),
+                        token['validation'],
+                    )
