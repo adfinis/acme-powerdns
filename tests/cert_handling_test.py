c49a5280b72754da042579fbb329d42fbdf5723f
Tobias Rueetschi <tobias.ruetschi@adfinis-sygroup.ch>
2017-02-07 15:27:44 +0100

    test cert_handling: update to class CertHandling changes

--- tests/cert_handling_test.py
+++ tests/cert_handling_test.py
@@ -11,29 +11,39 @@ from acme_powerdns import cert_handling
 
 @pytest.fixture(scope='session')
 def crth():
-    return cert_handling.CertHandling(
-        '.testenv/cert.csr',
-        '.testenv/cert.pem',
-    )
+    ch = cert_handling.CertHandling()
+    ch.set_csr('.testdata/csr/cert.csr')
+    ch.set_cert('.testdata/live/www.test.subpage.ch/cert.pem')
+    return ch
 
 
 @pytest.fixture(scope='session')
-def crth_fail():
-    return cert_handling.CertHandling(
-        '',
-        '',
-    )
+def crth_empty():
+    ch = cert_handling.CertHandling()
+    ch.set_csr('')
+    ch.set_cert('')
+    return ch
+
+
+@pytest.fixture(scope='session')
+def crth_none():
+    ch = cert_handling.CertHandling()
+    return ch
 
 
 def test_cert_handling_load_cert_req():
     with pytest.raises(ValueError):
-        assert type(crth_fail().load_cert_req()) == OpenSSL.crypto.X509Req
+        assert type(crth_empty().load_cert_req()) == OpenSSL.crypto.X509Req
+    with pytest.raises(ValueError):
+        assert type(crth_none().load_cert_req()) == OpenSSL.crypto.X509Req
     assert type(crth().load_cert_req()) == OpenSSL.crypto.X509Req
 
 
 def test_cert_handling_load_cert():
     with pytest.raises(ValueError):
-        assert type(crth_fail().load_cert()) == OpenSSL.crypto.X509
+        assert type(crth_empty().load_cert()) == OpenSSL.crypto.X509
+    with pytest.raises(ValueError):
+        assert type(crth_none().load_cert()) == OpenSSL.crypto.X509
     assert type(crth().load_cert()) == OpenSSL.crypto.X509
 
 
@@ -44,4 +54,3 @@ def test_cert_handling_enddate():
 def test_cert_handling_get_alternative_names():
     l = crth().get_alternative_names()
     assert type(l) == list
-    assert l == []
