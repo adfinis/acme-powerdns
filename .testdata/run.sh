14cf589d410062ded163c5446cba0d590eb63ace
Tobias Rueetschi <tobias.ruetschi@adfinis-sygroup.ch>
2017-02-07 15:49:34 +0100

    tests: create private keys, certificates and signing requests with new directory layout

--- .testdata/run.sh
+++ .testdata/run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+# cd to project directory
 cd "$(readlink -f "$(dirname "$0")/..")" || exit 1
 
+# create virtualenv
 if [ ! -e .testdata/.venv ]; then
     mkdir -p /host/.testdata/.venv
     python3 -m venv --without-pip .testdata/.venv
@@ -10,7 +12,10 @@ if [ ! -e .testdata/.venv ]; then
     python .testdata/get-pip.py
 fi
 
-if [ ! -f .testdata/cert.pem ]; then
+# create certificate stuff
+mkdir -p .testdata/www.example.com
+if [ ! -f .testdata/www.example.com/cert.pem ]; then
+
     cat <<__EOF__ > .testdata/x509.ext
 [req]
 distinguished_name = req_distinguished_name
@@ -35,8 +40,12 @@ subjectAltName = @alt_names
 DNS.1 = www.example.com
 DNS.2 = mail.example.com
 __EOF__
-    openssl genrsa -out .testdata/privkey.pem 4096
-    openssl req -new -config .testdata/x509.ext -key .testdata/privkey.pem -sha256 -out .testdata/cert.csr
+
+    openssl genrsa -out .testdata/account.key 4096
+    openssl req -new -sha256 -nodes -newkey rsa:4096 \
+        -config .testdata/x509.ext \
+        -keyout .testdata/live/www.example.com/privkey.pem \
+        -out .testdata/csr/www.example.com.csr
 fi
 
 make test
